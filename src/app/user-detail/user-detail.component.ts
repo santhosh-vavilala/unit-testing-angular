607f6d4cf75301c6ef0d73ea69a9310d94c25936
santhosh-vavilala <santhosh.vavilala@hotmail.com>
2019-01-01 21:10:51 +0530

    Add Service method to get users

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DbContextService } from '../services/db-context.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dbContextService: DbContextService) { }
 
   ngOnInit() {
+    this.dbContextService.getUsers().subscribe(x => { 
+      console.log(x);
+    });;
   }
 
 }
